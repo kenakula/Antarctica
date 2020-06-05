08cdf79d5fc0f04bb0b5052109928fc2f04e2716
Kenan Agaev <kenkosh@yandex.ru>
2020-06-05 17:17:11 +0300

    малые правки

--- build/js/vendor.js
+++ build/js/vendor.js
@@ -1,5 +1,32 @@
 'use strict';
 
 (function () {
+  var pageLogo = document.querySelector('.page-header__logo');
+  var menuToggler = document.querySelector('.main-nav__button');
+  var menuList = document.querySelector('.main-nav__list');
+
+  var activateJs = function () {
+    document.body.classList.remove('no-js');
+    pageLogo.classList.remove('page-header__logo--dark');
+    menuList.classList.remove('main-nav__list--expanded');
+  };
+
+  var openMenu = function () {
+    menuToggler.classList.add('main-nav__button--close');
+    pageLogo.classList.add('page-header__logo--dark');
+    menuList.classList.add('main-nav__list--expanded');
+  };
+
+  var closeMenu = function () {
+    menuToggler.classList.remove('main-nav__button--close');
+    pageLogo.classList.remove('page-header__logo--dark');
+    menuList.classList.remove('main-nav__list--expanded');
+  };
+
+  window.vendor = {
+    activateJs: activateJs,
+    openMenu: openMenu,
+    closeMenu: closeMenu,
+  }
 
 })();
--- source/js/main.js
+++ source/js/main.js
@@ -1,26 +1,20 @@
 'use strict';
 
 (function () {
-  var pageLogo = document.querySelector('.page-header__logo');
   var menuToggler = document.querySelector('.main-nav__button');
-  var menuList = document.querySelector('.main-nav__list');
 
   if (document.body.classList.contains('no-js')) {
-    document.body.classList.remove('no-js');
-    pageLogo.classList.remove('page-header__logo--dark');
-    menuList.classList.remove('main-nav__list--expanded');
+    window.vendor.activateJs();
   }
 
-  menuToggler.addEventListener('click', function () {
+  var onTogglerClickMenuToggle = function () {
     if (!menuToggler.classList.contains('main-nav__button--close')) {
-      menuToggler.classList.add('main-nav__button--close');
-      pageLogo.classList.add('page-header__logo--dark');
-      menuList.classList.add('main-nav__list--expanded');
+      window.vendor.openMenu();
     } else {
-      menuToggler.classList.remove('main-nav__button--close');
-      pageLogo.classList.remove('page-header__logo--dark');
-      menuList.classList.remove('main-nav__list--expanded');
+      window.vendor.closeMenu();
     }
-  });
+  }
+
+  menuToggler.addEventListener('click', onTogglerClickMenuToggle);
 
 })();
